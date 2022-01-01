1e2a54663eaa794bb461e6476112ccf401624a28
MAFIA <mosaabmograbe@yahoo.com>
2022-01-01 19:06:39 +0200

    finshing scrpit

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const audioElement = document.getElementById('audio');
 // VoiceRSS Javascript SDK
 const VoiceRSS={speech(e){this._validate(e),this._request(e)},_validate(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){let a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw`The browser does not support the audio codec ${e.c}`}},_request(e){const a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;let e=t.responseText;audioElement.src=e,audioElement.onloadedmetadata=(()=>{audioElement.play()})}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest(e){const a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return`key=${e.key||""}&src=${e.src||""}&hl=${e.hl||""}&r=${e.r||""}&c=${a||""}&f=${e.f||""}&ssml=${e.ssml||""}&b64=true`},_detectCodec(){const e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
-function test() {
- VoiceRSS.speech({
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
      key: 'b5d795e85f304acdaccd7b169c5f7818',
-     src: 'Hello, world!',
+     src: `${joke}`,
      hl: 'en-us',
      v: 'Linda',
      r: 0, 
@@ -16,5 +21,29 @@ function test() {
      ssml: false
  });
 }
+// Get Joke From JokeApi
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=twopart';
+    try{
+     const response = await fetch(apiUrl);
+     const data = await response.json();
+    // Handling Type -> towpart & single
+     if(data.setup){
+         joke = `${data.setup} ... ${data.delivery}`;
+     }else {
+         joke = data.joke;
+     }
+     // text-to-speech
+     tellMe(joke);
+     // Button disble;
+     toggleButton();
+    }catch(error) {
+        //to catch error here
+      console.log('fetch filed !',error);
+    }
+}
 
-test();
+// add Event Listener To Button When I Cliked It
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
